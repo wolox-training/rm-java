22b019b018d8f79c72e0c94ffafc6365f02c26e5
Millauro <A126056@bf.arg.igrupobbva>
2020-04-27 20:43:12 -0600

    Se actualiza en base a PR sobre TrainingApplication.java y pom.xml

--- src/main/java/wolox/training/TrainingApplication.java
+++ src/main/java/wolox/training/TrainingApplication.java
@@ -2,11 +2,7 @@ package wolox.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("wolox.training") 
-@EntityScan("wolox.training.models")
 @SpringBootApplication
 public class TrainingApplication {
 
