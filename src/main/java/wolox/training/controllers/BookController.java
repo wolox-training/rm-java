45d61e47661a6e501e3cfd88db591290ef5d1a6b
Millauro <A126056@bf.arg.igrupobbva>
2020-04-28 23:27:08 -0600

    Se corrije el manejo de excepciones. Tambien, el constructor del modelo Book (faltaba subtitulo)

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -1,6 +1,5 @@
 package wolox.training.controllers;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/wolox/training/exceptions/RestExceptionHandler.java
+++ src/main/java/wolox/training/exceptions/RestExceptionHandler.java
@@ -16,12 +16,13 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler({ BookNotFoundException.class })
 	protected ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
-		return handleExceptionInternal(ex, "Book not found", new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+		return handleExceptionInternal(ex, ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 
 	@ExceptionHandler({ BookIdMismatchException.class, ConstraintViolationException.class,
 	        DataIntegrityViolationException.class })
 	public ResponseEntity<Object> handleBadRequest(Exception ex, WebRequest request) {
+		System.out.println(ex.getLocalizedMessage());
 		return handleExceptionInternal(ex, ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,
 		        request);
 	}
