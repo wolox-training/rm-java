8cfc9651bc165623313351541897f392be22fe4e
Millauro <A126056@bf.arg.igrupobbva>
2020-04-30 12:53:13 -0600

    Se crea UserRepository buscando por Username

---
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package wolox.training.repositories;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wolox.training.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	  Optional<User> findFirstByUsernameOrderByIdAsc(String username);
+
+	}
