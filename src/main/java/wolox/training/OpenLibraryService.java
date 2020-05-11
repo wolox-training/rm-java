64da36e4353621b645878fb481eca7d3252e2fc4
Millauro <A126056@bf.arg.igrupobbva>
2020-05-11 15:47:50 -0600

    Se agrega busqueda de book por isbn

--- src/main/java/wolox/training/OpenLibraryService.java
+++ src/main/java/wolox/training/OpenLibraryService.java
@@ -1,11 +1,15 @@
 package wolox.training;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
+
+import wolox.training.models.Book;
+import wolox.training.models.OpenLibraryBook;
 
 public class OpenLibraryService {
 
@@ -17,6 +21,12 @@ public class OpenLibraryService {
 		        }, isbn).getBody();
 		OpenLibraryBook openLibraryBook = response.get("ISBN:" + isbn);
 		return Optional.ofNullable(openLibraryBook);
+	}
+
+	public Optional<OpenLibraryBook> mapBookInfo(Book b) {
+		OpenLibraryBook ol = new OpenLibraryBook(b.getIsbn(), b.getTitle(), b.getSubtitle(), b.getPublisher(),
+		        b.getYear(), b.getPages(), Arrays.asList(b.getAuthor()));
+		return Optional.ofNullable(ol);
 	}
 
 }
--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -3,6 +3,7 @@ package wolox.training.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,64 +15,77 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
 
+import wolox.training.OpenLibraryService;
 import wolox.training.exceptions.BookIdMismatchException;
 import wolox.training.exceptions.BookNotFoundException;
 import wolox.training.models.Book;
-import wolox.training.repositories.*;
+import wolox.training.models.OpenLibraryBook;
+import wolox.training.repositories.BookRepository;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+	        Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
-	
+
 	@Autowired
-    private BookRepository bookRepository;
- 
+	private BookRepository bookRepository;
+
 	@GetMapping
-    public Iterable<Book> findAll() {
-        return bookRepository.findAll();
-    }
-	
+	public Iterable<Book> findAll() {
+		return bookRepository.findAll();
+	}
+
 	@GetMapping("/")
 	@RequestMapping(params = "author")
-    public Optional<Book> findByAuthor(@RequestParam(required = true) String author) {
-        return bookRepository.findFirstByAuthorOrderByIdAsc(author);
-    }
- 
-    @GetMapping("/{id}")
-    public Book findOne(@PathVariable Long id) {
-        return bookRepository.findById(id)
-          .orElseThrow( () ->new BookNotFoundException("No hay libro para el id " + id) );
-    }
- 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Book create(@RequestBody Book book) {
-        return bookRepository.save(book);
-    }
- 
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        bookRepository.findById(id)
-          .orElseThrow( () ->new BookNotFoundException("No hay libro para el id " + id));
-        bookRepository.deleteById(id);
-    }
- 
-    @PutMapping("/{id}")
-    public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
-        if (book.getId() != id) {
-          throw new BookIdMismatchException("No hay libro para el id " + id);
-        }
-        bookRepository.findById(id)
-          .orElseThrow( () ->new BookNotFoundException("No hay libro para el id " + id));
-        return bookRepository.save(book);
-    }
+	public Optional<Book> findByAuthor(@RequestParam(required = true) String author) {
+		return bookRepository.findFirstByAuthorOrderByIdAsc(author);
+	}
 
+	@GetMapping("/{id}")
+	public Book findOne(@PathVariable Long id) {
+		return bookRepository.findById(id)
+		        .orElseThrow(() -> new BookNotFoundException("No hay libro para el id " + id));
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Book create(@RequestBody Book book) {
+		return bookRepository.save(book);
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException("No hay libro para el id " + id));
+		bookRepository.deleteById(id);
+	}
+
+	@PutMapping("/{id}")
+	public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
+		if (book.getId() != id) {
+			throw new BookIdMismatchException("No hay libro para el id " + id);
+		}
+		bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException("No hay libro para el id " + id));
+		return bookRepository.save(book);
+	}
+
+	@GetMapping("/")
+	@RequestMapping(params = "isbn")
+	public Optional<OpenLibraryBook> findByIsbn(@RequestParam(required = true) String isbn) {
+
+		OpenLibraryService ol = new OpenLibraryService();
+		Optional<Book> b = bookRepository.findFirstByIsbnOrderByIdAsc(isbn);
+		if (b.isPresent()) {
+			Book bb = b.orElseThrow(() -> new BookNotFoundException("Error de consistencia para el ISBN: " + isbn));
+			return ol.mapBookInfo(bb);
+		} else {
+			return ol.bookInfo(isbn);
+		}
+	}
 }
