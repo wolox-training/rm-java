19a8b9d7129c59c4c7e17e0a6528d07bba035e10
Millauro <A126056@bf.arg.igrupobbva>
2020-05-14 17:53:19 -0600

    Se agregan metodos complejos a book y user

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,11 +1,13 @@
 package wolox.training.repositories;
-import java.util.Optional;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-	  Optional<Book> findFirstByAuthorOrderByIdAsc(String author);
+    Optional<Book> findFirstByAuthorOrderByIdAsc(String author);
 
-	}
+    Iterable<Book> findAllByPublisherAndGenreAndYear(String publisher, String genere, String year);
+
+}
--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,11 +1,15 @@
 package wolox.training.repositories;
-import java.util.Optional;
 
+import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	  Optional<User> findFirstByUsernameOrderByIdAsc(String username);
+    Optional<User> findFirstByUsernameOrderByIdAsc(String username);
 
-	}
+    Iterable<User> findAllByBirthdateBetweenAndNameIgnoreCaseContaining(LocalDate birthdateIni,
+            LocalDate birthdateEnd, String name);
+
+}
