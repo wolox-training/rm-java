0ea1f0bedd0f7a07e514eee8a2a90909c0197277
Millauro <A126056@bf.arg.igrupobbva>
2020-05-15 18:23:55 -0600

    Se agregan queries custom a repo de book y user

--- src/main/java/wolox/training/controllers/UserController.java
+++ src/main/java/wolox/training/controllers/UserController.java
@@ -47,16 +47,13 @@ public class UserController {
 
     @GetMapping
     @RequestMapping(params = {"birthdateIni", "birthdateEnd", "name"})
-
     public Iterable<User> findByBirthdateAndName(
-            @RequestParam(required = true) @DateTimeFormat(iso = ISO.DATE) LocalDate birthdateIni,
-            @RequestParam(required = true) @DateTimeFormat(iso = ISO.DATE) LocalDate birthdateEnd,
-            @RequestParam(required = true) String name) {
+            @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE) LocalDate birthdateIni,
+            @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE) LocalDate birthdateEnd,
+            @RequestParam(required = false) String name) {
         return userRepository.findAllByBirthdateBetweenAndNameIgnoreCaseContaining(birthdateIni,
                 birthdateEnd, name);
-
     }
-
 
     @GetMapping("/{id}")
     public User findOne(@PathVariable Long id) {
--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,18 +1,19 @@
 package wolox.training.repositories;
 
 import java.util.Optional;
-
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import wolox.training.models.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Optional<Book> findFirstByAuthorOrderByIdAsc(String author);
 
-    Iterable<Book> findAllByPublisherAndGenreAndYear(String publisher, String genere, String year);
+    @Query("SELECT u FROM Book u WHERE (:pub <= '' or u.publisher = :pub) and (:gen <= '' or"
+        + " u.genre = :gen) and (:yea <= '' or u.year = :yea)")
+    Iterable<Book> findAllByPublisherAndGenreAndYear(@Param("pub") String publisher, @Param("gen") String genre, @Param("yea") String year);
 
-  	Optional<Book> findFirstByIsbnOrderByIdAsc(String isbn);
+    Optional<Book> findFirstByIsbnOrderByIdAsc(String isbn);
 
 }
